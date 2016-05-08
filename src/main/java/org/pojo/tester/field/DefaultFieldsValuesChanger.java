ae51956348f70828145f936d0f2cc64a1fc59359
Piotr Joński <yoyo@wp.eu>
2016-05-08 15:56:55 +0200

    Dodanie bezargumentowego prywatnego konstruktora

--- src/main/java/org/pojo/tester/field/DefaultFieldsValuesChanger.java
+++ src/main/java/org/pojo/tester/field/DefaultFieldsValuesChanger.java
@@ -1,8 +1,11 @@
 package org.pojo.tester.field;
 
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.pojo.tester.field.primitive.AbstractPrimitiveValueChanger;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DefaultFieldsValuesChanger {
 
     public static final AbstractFieldsValuesChanger INSTANCE = AbstractPrimitiveValueChanger.getInstance()
