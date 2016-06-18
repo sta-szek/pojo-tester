f8568ddca052f249275d417957b7cd657f636953
Piotr Joński <yoyo@wp.eu>
2016-06-18 16:55:50 +0200

    Refaktor

--- src/main/java/org/pojo/tester/field/DefaultFieldValueChanger.java
+++ src/main/java/org/pojo/tester/field/DefaultFieldValueChanger.java
@@ -4,13 +4,11 @@ package org.pojo.tester.field;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.pojo.tester.field.collections.CollectionsFieldValueChanger;
-import org.pojo.tester.field.collections.iterators.AbstractIteratorsFieldValueChanger;
 import org.pojo.tester.field.primitive.AbstractPrimitiveValueChanger;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DefaultFieldValueChanger {
 
     public static final AbstractFieldValueChanger INSTANCE = new EnumValueChanger().register(AbstractPrimitiveValueChanger.INSTANCE)
-                                                                                   .register(CollectionsFieldValueChanger.INSTANCE)
-                                                                                   .register(AbstractIteratorsFieldValueChanger.INSTANCE);
+                                                                                   .register(CollectionsFieldValueChanger.INSTANCE);
 }
--- src/main/java/org/pojo/tester/field/collections/CollectionsFieldValueChanger.java
+++ src/main/java/org/pojo/tester/field/collections/CollectionsFieldValueChanger.java
@@ -3,10 +3,12 @@ package org.pojo.tester.field.collections;
 
 import org.pojo.tester.field.AbstractFieldValueChanger;
 import org.pojo.tester.field.collections.collection.AbstractCollectionFieldValueChanger;
+import org.pojo.tester.field.collections.iterators.AbstractIteratorsFieldValueChanger;
 import org.pojo.tester.field.collections.map.AbstractMapFieldValueChanger;
 
 public class CollectionsFieldValueChanger {
     public static final AbstractFieldValueChanger INSTANCE = new ArrayValueChanger().register(new StreamValueChanger())
                                                                                     .register(AbstractCollectionFieldValueChanger.INSTANCE)
-                                                                                    .register(AbstractMapFieldValueChanger.INSTANCE);
+                                                                                    .register(AbstractMapFieldValueChanger.INSTANCE)
+                                                                                    .register(AbstractIteratorsFieldValueChanger.INSTANCE);
 }
