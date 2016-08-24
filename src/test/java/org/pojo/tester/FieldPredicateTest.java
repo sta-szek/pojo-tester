caac30a8b20622aad30e5021c2bf690fed690d0a
Piotr Joński <yoyo@wp.eu>
2016-08-24 10:02:23 +0200

    Update libraries and test refactoring

--- src/test/java/org/pojo/tester/FieldPredicateTest.java
+++ src/test/java/org/pojo/tester/FieldPredicateTest.java
@@ -1,6 +1,5 @@
 package org.pojo.tester;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -13,9 +12,9 @@ import org.junit.runner.RunWith;
 import org.pojo.tester.utils.FieldUtils;
 import test.predicate.TestPredicate;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
-import static org.pojo.tester.PredicateAssertions.assertThat;
 import static test.TestHelper.getDefaultDisplayName;
 
 @RunWith(JUnitPlatform.class)
@@ -30,7 +29,7 @@ public class FieldPredicateTest {
         final Predicate<String> predicate = FieldPredicate.includeAllFields(TestPredicate.class);
 
         // then
-        assertThat(predicate).accepts(allFieldNames);
+        assertThat(predicate).acceptsAll(allFieldNames);
     }
 
     @TestFactory
@@ -42,6 +41,16 @@ public class FieldPredicateTest {
                                                Should_Return_Predicate_That_Accept_List_Of_Field_Names(value)));
     }
 
+    public Executable Should_Return_Predicate_That_Accept_List_Of_Field_Names(final List<String> includedFields) {
+        return () -> {
+            // when
+            final Predicate<String> predicate = FieldPredicate.include(includedFields);
+
+            // then
+            assertThat(predicate).acceptsAll(includedFields);
+        };
+    }
+
     @TestFactory
     public Stream<DynamicTest> Should_Return_Predicate_That_Accept_Array_Of_Field_Names() {
         return Stream.of(new String[]{"a"},
@@ -51,6 +60,16 @@ public class FieldPredicateTest {
                                                Should_Return_Predicate_That_Accept_Array_Of_Field_Names(value)));
     }
 
+    public Executable Should_Return_Predicate_That_Accept_Array_Of_Field_Names(final String... includedFields) {
+        return () -> {
+            // when
+            final Predicate<String> predicate = FieldPredicate.include(includedFields);
+
+            // then
+            assertThat(predicate).accepts(includedFields);
+        };
+    }
+
     @TestFactory
     public Stream<DynamicTest> Should_Return_Predicate_That_Does_Not_Accept_List_Of_Fields() {
         return Stream.of(newArrayList("a"),
@@ -58,6 +77,16 @@ public class FieldPredicateTest {
                          newArrayList("a", "b", "c"))
                      .map(value -> dynamicTest(getDefaultDisplayName(value),
                                                Should_Return_Predicate_That_Does_Not_Accept_List_Of_Fields(value)));
+    }
+
+    public Executable Should_Return_Predicate_That_Does_Not_Accept_List_Of_Fields(final List<String> excludedFields) {
+        return () -> {
+            // when
+            final Predicate<String> predicate = FieldPredicate.exclude(excludedFields);
+
+            // then
+            assertThat(predicate).rejectsAll(excludedFields);
+        };
     }
 
     @TestFactory
@@ -71,81 +100,14 @@ public class FieldPredicateTest {
                                                Should_Return_Predicate_That_Does_Not_Accept_Array_Of_Fields(value)));
     }
 
-    private Executable Should_Return_Predicate_That_Does_Not_Accept_Array_Of_Fields(final String... excludedFields) {
+    public Executable Should_Return_Predicate_That_Does_Not_Accept_Array_Of_Fields(final String... excludedFields) {
         return () -> {
             // when
             final Predicate<String> predicate = FieldPredicate.exclude(excludedFields);
 
             // then
-            assertThat(predicate).doesNotAccept(excludedFields);
+            assertThat(predicate).rejects(excludedFields);
         };
     }
 
-    private Executable Should_Return_Predicate_That_Does_Not_Accept_List_Of_Fields(final List<String> excludedFields) {
-        return () -> {
-            // when
-            final Predicate<String> predicate = FieldPredicate.exclude(excludedFields);
-
-            // then
-            assertThat(predicate).doesNotAccept(excludedFields);
-        };
-    }
-
-    private Executable Should_Return_Predicate_That_Accept_Array_Of_Field_Names(final String... includedFields) {
-        return () -> {
-            // when
-            final Predicate<String> predicate = FieldPredicate.include(includedFields);
-
-            // then
-            assertThat(predicate).accepts(includedFields);
-        };
-    }
-
-    private Executable Should_Return_Predicate_That_Accept_List_Of_Field_Names(final List<String> includedFields) {
-        return () -> {
-            // when
-            final Predicate<String> predicate = FieldPredicate.include(includedFields);
-
-            // then
-            assertThat(predicate).accepts(includedFields);
-        };
-    }
-
-}
-
-class PredicateAssertions {
-
-    private final Predicate<String> predicate;
-
-    private PredicateAssertions(final Predicate<String> predicate) {
-        this.predicate = predicate;
-    }
-
-    static PredicateAssertions assertThat(final Predicate<String> predicate) {
-        return new PredicateAssertions(predicate);
-    }
-
-    void doesNotAccept(final String[] values) {
-        doesNotAccept(Arrays.asList(values));
-    }
-
-    void doesNotAccept(final List<String> values) {
-        values.forEach(value -> {
-            final boolean result = predicate.test(value);
-            org.assertj.core.api.Assertions.assertThat(result)
-                                           .isFalse();
-        });
-    }
-
-    void accepts(final String[] values) {
-        accepts(Arrays.asList(values));
-    }
-
-    void accepts(final List<String> values) {
-        values.forEach(value -> {
-            final boolean result = predicate.test(value);
-            org.assertj.core.api.Assertions.assertThat(result)
-                                           .isTrue();
-        });
-    }
 }
