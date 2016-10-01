107b741d47bc3e5116dae9cc7122d6186a48856c
Piotr Joński <yoyo@wp.eu>
2016-10-01 23:21:50 +0200

    #70. Updated readme

--- docs/gitbook/gitbook-plugin-editlink/plugin.js
+++ docs/gitbook/gitbook-plugin-editlink/plugin.js
@@ -1,9 +1,9 @@
 require(["gitbook", "jQuery"], function(gitbook, $) {
   gitbook.events.bind('start', function (e, config) {
-      var conf = config.editlink;
-      var label = conf.label;
-      var base = conf.base;
-      var multilingual = conf.multilingual || false;
+    var conf = config.editlink
+    var label = conf.label
+    var base = conf.base
+    var multilingual = conf.multilingual || false
 
     if (base.slice(-1) !== "/") {
       base += "/"
@@ -13,11 +13,11 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
       icon: 'fa fa-edit',
       text: label,
       onClick: function() {
-          var filepath = gitbook.state.filepath;
-          var lang = multilingual && $('html').attr('lang') ? $('html').attr('lang') + '/' : '';
-
+        var filepath = gitbook.state.filepath
+        var lang = multilingual && $('html').attr('lang') ? $('html').attr('lang') + '/' : ''
+        
         window.open(base + lang + filepath)
       }
     })
   })
-});
+})
