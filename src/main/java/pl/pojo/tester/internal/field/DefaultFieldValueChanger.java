f80ce44a8ff12d2ccca18018c63055c00e338af1
Piotr Joński <yoyo@wp.eu>
2016-10-08 21:25:56 +0200

    #133. Formatting

--- src/main/java/pl/pojo/tester/internal/field/DefaultFieldValueChanger.java
+++ src/main/java/pl/pojo/tester/internal/field/DefaultFieldValueChanger.java
@@ -9,10 +9,8 @@ import pl.pojo.tester.internal.field.primitive.AbstractPrimitiveValueChanger;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DefaultFieldValueChanger {
 
-    public static final AbstractFieldValueChanger INSTANCE = new EnumValueChanger().attachNext(
-            AbstractPrimitiveValueChanger.INSTANCE)
-                                                                                   .attachNext(
-                                                                                           CollectionsFieldValueChanger.INSTANCE)
-                                                                                   .attachNext(new StringValueChanger
-                                                                                                       ());
+    public static final AbstractFieldValueChanger INSTANCE = new EnumValueChanger()
+            .attachNext(AbstractPrimitiveValueChanger.INSTANCE)
+            .attachNext(CollectionsFieldValueChanger.INSTANCE)
+            .attachNext(new StringValueChanger());
 }
