73f910cc72a9d901e388f50b51b7090e19f8ad9e
Piotr Joński <yoyo@wp.eu>
2016-10-17 20:02:38 +0200

    #120. Script modifications

--- deployPages.sh
+++ deployPages.sh
@@ -15,12 +15,17 @@ then
   exit 0
 fi
 
-echo "1/4 CLONE POJO-TESTER REPOSITORY"
-echo ${POJO_TESTER_REPO}
-curl -u ${TRAVIS_DEPLOY_GH_PAGES_TOKEN}:x-oauth-basic https://api.github.com/user
+
 rev=$(git rev-parse --short HEAD)
 
-mkdir repo
+echo "1/4 GENERATE JAVADOCS"
+./gradlew javadoc >/dev/null
+
+echo "2/4 GENERATE GITBOOK"
+gitbook install ./src/book/ >/dev/null
+gitbook build ./src/book/ ./repo
+
+echo "3/4 PUBLISH PAGES"
 cd repo
 git init
 git config user.name "Piotr Joński"
@@ -28,27 +33,13 @@ git config user.email "yoyo@wp.eu"
 git remote add origin ${POJO_TESTER_REPO}
 git fetch origin
 git reset origin/gh-pages
-cd ..
-
-echo "2/4 GENERATE JAVADOCS"
-./gradlew javadoc >/dev/null
-
-echo "3/4 GENERATE GITBOOK"
-gitbook install ./src/book/ >/dev/null
-gitbook build ./src/book/ ./repo
-git status -s
-ls -al
-
-echo "4/4 PUBLISH PAGES"
-cd repo
-git status -s
-ls -al
-
-git config user.name "Piotr Joński"
-git config user.email "yoyo@wp.eu"
 
 git remote -v
+git status -s
+ls -al
+
 git add -A .
+git status -s
 git commit -m "Rebuild pojo-tester pages at ${rev}" >/dev/null
 pwd
 git status -s
