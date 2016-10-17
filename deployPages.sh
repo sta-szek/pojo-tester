bf699e49bea71a938872bb0604b9df94cfc7f719
Piotr Joński <yoyo@wp.eu>
2016-10-17 19:05:48 +0200

    #120. Script modifications

--- deployPages.sh
+++ deployPages.sh
@@ -7,7 +7,7 @@ TRAVIS_BRANCH="master"
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
-POJO_TESTER_REPO="https://$TRAVIS_DEPLOY_GH_PAGES_TOKEN@github.com/sta-szek/pojo-tester.git"
+POJO_TESTER_REPO="https://$TRAVIS_DEPLOY_GH_PAGES_TOKEN:x-oauth-basic@github.com/sta-szek/pojo-tester.git"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 then
