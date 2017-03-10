cdcc49c6ff17c083135dc7a52e33149ef83f1b74
Piotr Joński <yoyo@wp.eu>
2017-03-10 21:34:32 +0100

    #187. Improved integration test timing by reducing number of iterations

--- src/integrationTest/java/pl/pojo/tester/issue187/EntityClassPojoTest.java
+++ src/integrationTest/java/pl/pojo/tester/issue187/EntityClassPojoTest.java
@@ -2,6 +2,12 @@ package pl.pojo.tester.issue187;
 
 
 import org.junit.jupiter.api.Test;
+import pl.pojo.tester.api.ConstructorParameters;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
 import static pl.pojo.tester.api.assertion.Method.*;
@@ -10,8 +16,13 @@ public class EntityClassPojoTest {
 
     @Test
     public void Should_Test_Entity_Class() {
-        for (int i = 0; i < 10000; i++) {
+        final ConstructorParameters constructorParameters = new ConstructorParameters(
+                new Object[]{ LocalDateTime.now(), ZoneOffset.MAX, ZoneId.systemDefault() },
+                new Class[]{ LocalDateTime.class, ZoneOffset.class, ZoneId.class });
+
+        for (int i = 0; i < 1000; i++) {
             assertPojoMethodsFor(EntityClass.class).testing(GETTER, TO_STRING, CONSTRUCTOR, EQUALS, HASH_CODE)
+                                                   .create(ZonedDateTime.class, constructorParameters)
                                                    .areWellImplemented();
         }
     }
