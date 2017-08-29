2742b949f06c5a6699e3f2d9f45827b1aa177199
Your Name <you@example.com>
2017-08-30 01:35:16 +0300

    More Codacy stuff (2)

--- src/main/java/pl/pojo/tester/internal/utils/Sublists.java
+++ src/main/java/pl/pojo/tester/internal/utils/Sublists.java
@@ -7,20 +7,22 @@ import java.util.List;
 public final class Sublists {
     private Sublists() {
     }
+
     /**
      * Given a list of objects, return a list of sublists, i-th sublist includes i-th element of
      * the original list and all elements that follow it in the original list.
+     * All returned lists are read-only and backed by a copy of the original list, so later changes
+     * in the original list will not affect the returned sublists.
      *
-     * @param list0 list of objects
+     * @param list list of objects
      * @param <T>   the type of list element
-     * @return list of sublists of list0
+     * @return sublists of list
      */
-    public static <T> List<List<T>> subsequences(final List<T> list0) {
-        final List<T> list = new ArrayList<>(list0);
+    public static <T> List<List<T>> subsequences(final List<T> list) {
+        final List<T> copyOfList = new ArrayList<>(list);
         final List<List<T>> res = new ArrayList<>();
-        for (int i = 0,
-             n = list.size(); i < n; i++) {
-            res.add(Collections.unmodifiableList(list.subList(i, n)));
+        for (int i = 0; i < copyOfList.size(); i++) {
+            res.add(Collections.unmodifiableList(copyOfList.subList(i, copyOfList.size())));
         }
         return Collections.unmodifiableList(res);
     }
