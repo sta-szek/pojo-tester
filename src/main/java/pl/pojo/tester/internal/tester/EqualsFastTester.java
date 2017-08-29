fdda0b8a64d46116bc990a6d5e96e47f9b6c92db
Your Name <you@example.com>
2017-08-29 23:36:06 +0300

    Fixed Codacy stuff

--- src/main/java/pl/pojo/tester/internal/tester/EqualsFastTester.java
+++ src/main/java/pl/pojo/tester/internal/tester/EqualsFastTester.java
@@ -4,13 +4,13 @@ import pl.pojo.tester.internal.field.AbstractFieldValueChanger;
 
 public class EqualsFastTester extends EqualsTester {
 
-  public EqualsFastTester() {
-    super();
-    setThoroughTesting(false);
-  }
+    public EqualsFastTester() {
+        super();
+        setThoroughTesting(false);
+    }
 
-  public EqualsFastTester(AbstractFieldValueChanger abstractFieldValueChanger) {
-    super(abstractFieldValueChanger);
-    setThoroughTesting(false);
-  }
+    public EqualsFastTester(AbstractFieldValueChanger abstractFieldValueChanger) {
+        super(abstractFieldValueChanger);
+        setThoroughTesting(false);
+    }
 }
